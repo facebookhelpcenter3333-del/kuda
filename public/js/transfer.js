6bc661b77277db36c7064b50f68ac6ae1f2519e0
facebookhelpcenter3333-del <facebookhelpcenter3333@gmail.com>
2026-05-21 09:29:24 -0700

    Updated transfer.js

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -259,7 +259,7 @@ async function completeTransfer() {
     setTimeout(() => {
         document.getElementById('loadingOverlay').classList.add('hidden');
         window.location.href = '/success.html';
-    }, 6000);
+    }, 4000);
 }
 
 // Generate reference number
